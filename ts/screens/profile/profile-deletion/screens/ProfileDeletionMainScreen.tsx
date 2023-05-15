7180c7088bc661ff6af9533afb3bdbf6e4a157b3
Alessandro Izzo <alessandro.izzo@pagopa.it>
2023-05-15 15:31:03 +0200

    feat: Add Profile deletion main screen and localized strings used

--- ts/screens/profile/profile-deletion/screens/ProfileDeletionMainScreen.tsx
+++ ts/screens/profile/profile-deletion/screens/ProfileDeletionMainScreen.tsx
@@ -1,20 +1,67 @@
 import React from "react";
+import { View } from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { H1 } from "../../../../components/core/typography/H1";
-import TopScreenComponent from "../../../../components/screens/TopScreenComponent";
 import I18n from "../../../../i18n";
 import { emptyContextualHelp } from "../../../../utils/emptyContextualHelp";
+import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
+import { ProfileDeletionRoutes } from "../navigation/routes";
+import FooterWithButtons from "../../../../components/ui/FooterWithButtons";
+import {
+  cancelButtonProps,
+  confirmButtonProps
+} from "../../../../features/bonus/bonusVacanze/components/buttons/ButtonConfigurations";
+import ScreenContent from "../../../../components/screens/ScreenContent";
+import { IOStyles } from "../../../../components/core/variables/IOStyles";
+import { InfoBox } from "../../../../components/box/InfoBox";
+import { Label } from "../../../../components/core/typography/Label";
+import { IOColors } from "../../../../components/core/variables/IOColors";
+import { Body } from "../../../../components/core/typography/Body";
+import { H4 } from "../../../../components/core/typography/H4";
 
-const screenTitle = I18n.t("profile.main.title");
+const screenTitle = I18n.t("profile.delete.title");
 
-const ProfileDeletionMainScreen = () => (
-  <TopScreenComponent
-    accessibilityLabel={screenTitle}
-    headerTitle={screenTitle}
-    contextualHelp={emptyContextualHelp}
-  >
-    <H1>TEST</H1>
-  </TopScreenComponent>
-);
+const ProfileDeletionMainScreen = () => {
+  const navigation = useNavigation();
+
+  const cancelButton = cancelButtonProps(
+    () => navigation.goBack(),
+    I18n.t("global.buttons.cancel")
+  );
+
+  const confirmButton = confirmButtonProps(
+    () => navigation.navigate(ProfileDeletionRoutes.PROFILE_DELETION_DETAILS),
+    I18n.t("global.buttons.continue")
+  );
+
+  return (
+    <BaseScreenComponent
+      accessibilityLabel={screenTitle}
+      headerTitle={screenTitle}
+      goBack
+      contextualHelp={emptyContextualHelp}
+    >
+      <SafeAreaView style={IOStyles.flex} edges={["bottom", "left", "right"]}>
+        <ScreenContent title={I18n.t("profile.delete.main.title")}>
+          <View style={IOStyles.horizontalContentPadding}>
+            <InfoBox
+              iconName={"io-warning"}
+              iconColor={IOColors["warning-500"]}
+            >
+              <H4>{I18n.t("global.genericAlert")}</H4>
+              <Body>{I18n.t("profile.delete.main.description")}</Body>
+            </InfoBox>
+          </View>
+        </ScreenContent>
+        <FooterWithButtons
+          type="TwoButtonsInlineHalf"
+          leftButton={cancelButton}
+          rightButton={confirmButton}
+        />
+      </SafeAreaView>
+    </BaseScreenComponent>
+  );
+};
 
 export default ProfileDeletionMainScreen;

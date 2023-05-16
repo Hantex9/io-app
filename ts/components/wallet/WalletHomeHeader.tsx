1c279b9a3d179242ad845ef71dbabf3035e27ec2
Alessandro Izzo <alessandro.izzo@pagopa.it>
2023-05-16 14:34:15 +0200

    feat: Add CTA to Profile deletion flow into `WalletHomeHeader`

--- ts/components/wallet/WalletHomeHeader.tsx
+++ ts/components/wallet/WalletHomeHeader.tsx
@@ -1,7 +1,10 @@
-import * as O from "fp-ts/lib/Option";
 import * as React from "react";
+import * as O from "fp-ts/lib/Option";
 import { View, FlatList, StyleSheet } from "react-native";
 import { connect } from "react-redux";
+import { useNavigation } from "@react-navigation/core";
+import { StackNavigationProp } from "@react-navigation/stack";
+
 import { navigateToAvailableBonusScreen } from "../../features/bonus/bonusVacanze/navigation/action";
 import I18n from "../../i18n";
 import NavigationService from "../../navigation/NavigationService";
@@ -21,6 +24,8 @@ import { IOStyles } from "../core/variables/IOStyles";
 import ItemSeparatorComponent from "../ItemSeparatorComponent";
 import TouchableDefaultOpacity from "../TouchableDefaultOpacity";
 import IconFont from "../ui/IconFont";
+import { ProfileDeletionParamsList } from "../../screens/profile/profile-deletion/navigation/params";
+import { ProfileDeletionRoutes } from "../../screens/profile/profile-deletion/navigation/routes";
 
 type Props = ReturnType<typeof mapStateToProps> &
   ReturnType<typeof mapDispatchToProps>;
@@ -47,10 +52,15 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "flex-start",
     justifyContent: "space-between"
+  },
+  fullWidth: {
+    width: "100%"
   }
 });
 
 const WalletHomeHeader = (props: Props) => {
+  const navigation =
+    useNavigation<StackNavigationProp<ProfileDeletionParamsList>>();
   const navigationListItems: ReadonlyArray<NavigationListItem> = [
     {
       title: I18n.t("wallet.paymentMethod"),
@@ -106,10 +116,13 @@ const WalletHomeHeader = (props: Props) => {
     315
   );
 
+  const startProfileDeletionFlow = () =>
+    navigation.navigate(ProfileDeletionRoutes.PROFILE_DELETION_MAIN);
+
   return (
     <View
       style={{
-        flexDirection: "row",
+        flexDirection: "column",
         alignItems: "center",
         justifyContent: "space-between",
         paddingHorizontal: 8
@@ -118,26 +131,49 @@ const WalletHomeHeader = (props: Props) => {
       <H1 color={"white"} accessible={true} accessibilityRole="header">
         {I18n.t("wallet.wallet")}
       </H1>
-      <TouchableDefaultOpacity
-        style={{
-          flexDirection: "row",
-          alignItems: "center"
-        }}
-        onPress={present}
-        accessible={true}
-        accessibilityLabel={I18n.t("wallet.accessibility.addElement")}
-        accessibilityRole="button"
-      >
-        <IconFont
-          name="io-plus"
-          color={IOColors.white}
-          size={customVariables.fontSize2}
-        />
-        <HSpacer size={8} />
-        <H4 color={"white"}>
-          {I18n.t("wallet.newPaymentMethod.add").toUpperCase()}
-        </H4>
-      </TouchableDefaultOpacity>
+      <View style={[IOStyles.rowSpaceBetween, IOStyles.flex, styles.fullWidth]}>
+        <TouchableDefaultOpacity
+          style={[IOStyles.row, IOStyles.alignCenter]}
+          onPress={startProfileDeletionFlow}
+          accessible={true}
+          accessibilityLabel={I18n.t(
+            "profile.main.privacy.removeAccount.details.cta"
+          )}
+          accessibilityRole="button"
+        >
+          <IconFont
+            name="io-trash"
+            color={IOColors.white}
+            size={customVariables.fontSize2}
+          />
+          <HSpacer size={8} />
+          <H4 color={"white"}>
+            {I18n.t(
+              "profile.main.privacy.removeAccount.details.cta"
+            ).toUpperCase()}
+          </H4>
+        </TouchableDefaultOpacity>
+        <TouchableDefaultOpacity
+          style={{
+            flexDirection: "row",
+            alignItems: "center"
+          }}
+          onPress={present}
+          accessible={true}
+          accessibilityLabel={I18n.t("wallet.accessibility.addElement")}
+          accessibilityRole="button"
+        >
+          <IconFont
+            name="io-plus"
+            color={IOColors.white}
+            size={customVariables.fontSize2}
+          />
+          <HSpacer size={8} />
+          <H4 color={"white"}>
+            {I18n.t("wallet.newPaymentMethod.add").toUpperCase()}
+          </H4>
+        </TouchableDefaultOpacity>
+      </View>
       {bottomSheet}
     </View>
   );
